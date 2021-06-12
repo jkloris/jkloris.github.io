1dd6669ae53710499dd246c89d7e498dd745afd6
jkloris <kapusta.jergus@gmail.com>
2021-06-12 23:28:32 +0200

    zapasy  12.6.

--- js_euro2020/results.js
+++ js_euro2020/results.js
@@ -4,8 +4,8 @@ matchResults = setMatches(teamsA, teamsB);
 
 matchResults[0].setResult(0,3);
 matchResults[1].setResult(1,1);
-// matchResults[2].setResult(2,1);
-// matchResults[3].setResult(1,1);
+matchResults[2].setResult(0,1);
+matchResults[3].setResult(3,0);
 // matchResults[4].setResult(9,2);
 // matchResults[5].setResult(1,1);
 // matchResults[6].setResult(0,3);
@@ -25,3 +25,4 @@ competitors.forEach((e)=>console.log(e.name + ": "+e.score));
 printMResults(matchResults);
 printScoreTable(competitors);
 
+alert("NOVINKA! Pozri si tikety ostatných jednym kliknutím. Stačí ísť na spodok stránky")
