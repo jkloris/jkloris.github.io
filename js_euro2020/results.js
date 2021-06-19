842c85a5d7adfd773d25817f3322a0789fc31300
jkloris <kapusta.jergus@gmail.com>
2021-06-19 11:51:16 +0200

    zapasy 18.6

--- js_euro2020/results.js
+++ js_euro2020/results.js
@@ -20,6 +20,9 @@ matchResults[14].setResult(3,0);
 matchResults[15].setResult(2,1);
 matchResults[16].setResult(1,2);
 matchResults[17].setResult(2,0);
+matchResults[18].setResult(1,0);
+matchResults[19].setResult(1,1);
+matchResults[20].setResult(0,0);
 
 //matchResults = nike.tips.m;
 //pocitanie bodov
